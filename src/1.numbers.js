843fd4350c8a9612a44061a9d25298061945f6e1
bonima21 <79886034+bonima21@users.noreply.github.com>
2022-08-30 21:41:25 -0400

    finished text string.js

--- src/1.numbers.js
+++ src/1.numbers.js
@@ -1,11 +1,11 @@
 
 function add (num1, num2) {
-  return parseInt(num1) + parseInt(num2) ;
+  return num1 + num2 ;
 }
 
 
 function subtract (num1, num2) {
-  return parseInt(num1) - parseInt(num2);
+  return num1 - num2;
 }
 
 
--- src/2.strings.js
+++ src/2.strings.js
@@ -1,27 +1,25 @@
-//reverseString: you should be able to reverse a string
-// Example: "abc" => "cba"
+
 function reverseString(str) {
-  /**
-   * can be achieved by combining: 
-   *    1. string.prototype.split
-   *    2. array.prototype.reverse
-   *    3. array.prototype.join
-   * 
-   * Can even be done on one line
-   */
-  return ;
+  
+  var splitString=str.split("");
+  var reverseArray=splitString.reverse();
+  var joinArray=reverseArray.join("");
+  return joinArray;
 }
 
 //capitalize: should return the input in all-caps
 // Example: "this is a string" => "THIS IS A STRING"
 function capitalize(str) {
-  return ;
+  const capitalizeString=str.toUpperCase()
+return capitalizeString;
+  
 }
 
 // splitString: should divide a string into substrings and return an array
 // Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
 function splitString(str, splitAt = `,`) {
-  return ;
+  const arrayOfString=str.split(splitAt)
+  return arrayOfString ;
 }
 module.exports = {
   reverseString,
