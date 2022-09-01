0278a6262d25f115b541d08abfb37f9dffc8d708
bonima21 <79886034+bonima21@users.noreply.github.com>
2022-09-01 12:04:53 -0400

    finished objects  array.js

--- src/4.objects.js
+++ src/4.objects.js
@@ -1,16 +1,27 @@
 /**
  * The following object must contain:
- *  `first_name`property that is of type string
- *  `last_name` property that is of type string
- *  `class_of` property that is of type number
+ *  `first_name`property that is of type string *
+ *  `last_name` property that is of type string *
+ *  `class_of` property that is of type number *
  *   a `full_name` function that would return the concatenation of `first_name` and `last_name` properties.
  *   an `introduction` function that returns a string of a welcome message. The introduction function:
  *      * must call the `full_name()` function to present the person's name.
  *      * must use the `class_of` property in the introduction
  */
 const personObject = {
+  first_name: "Mimi",
+  last_name:"Boni",
+  class_of:25,
 
+  full_name : function() {
+    return this.first_name + " " + this.last_name;
+  },
+  introduction : function() {
+      return "Welcome "+this.full_name()+". Your class is "+this.class_of;
+  }
 };
+
+console.log(personObject.introduction());
 
 module.exports = {
   personObject
